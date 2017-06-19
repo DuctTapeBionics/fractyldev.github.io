ba893d0e647d9a74bb3fb2a78d36d84063acde6e
TemporalFuzz <vanxmax@gmail.com>
2017-06-19 15:52:44 -0700

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,9 +1,14 @@
 function setup() {
   var canvas = document.getElementById("canvas");
   var ctx = canvas.getContext("2d");
-  if(
-  ctx.canvas.width  = Math.min(window.innerWidth, window.innerHeight);
-  ctx.canvas.height = 5/6 * Math.min(window.innerWidth, window.innerHeight);
+  if(window.innerWidth/window.innerHeight < 1.2) {
+    ctx.canvas.width  = window.innerWidth;
+    ctx.canvas.height = 5/6 * window.innerWidth;
+  }
+  else {
+    ctx.canvas.width  = window.innerHeight;
+    ctx.canvas.height = 5/6 * window.innerHeight;
+  }
 }
 document.addEventListener("DOMContentLoaded", function(event) { 
   setup();
