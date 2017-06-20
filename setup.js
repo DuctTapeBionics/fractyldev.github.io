55264587c69767b8cb2cecc9c66f8efee51a5f1b
TemporalFuzz <vanxmax@gmail.com>
2017-06-19 17:12:34 -0700

    Update setup.js
    Fixed the final bug because I am a code-illiterate dolt

--- setup.js
+++ setup.js
@@ -2,16 +2,14 @@ function setup() {
   var canvas = document.getElementById("canvas");
   var ctx = canvas.getContext("2d");
   if(window.innerWidth/window.innerHeight < 1.2) {
-    ctx.canvas.width  = window.innerWidth;
-    ctx.canvas.height = 5/6 * window.innerWidth;
+    canvas.width  = window.innerWidth;
+    canvas.height = 5/6 * window.innerWidth;
   }
   else {
-    ctx.canvas.width  = window.innerHeight;
-    ctx.canvas.height = 5/6 * window.innerHeight;
+    canvas.width  = window.innerHeight;
+    canvas.height = 5/6 * window.innerHeight;
   }
 }
 document.addEventListener("DOMContentLoaded", function(event) { 
   setup();
-  console.log(window.innerWidth);
-  console.log(window.innerHeight);
 });//When the document is loaded, resize the canvas
