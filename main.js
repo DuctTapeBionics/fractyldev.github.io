6a6a4f3d2e46c6b70aaa1b79f02630d405b9bfb1
TemporalFuzz <vanxmax@gmail.com>
2017-06-20 15:20:26 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 var player = new Player([200, 200]);
 var blocks = [];
-/*blocks.push(new Block({ pos: [200, 220] }));
+blocks.push(new Block({ pos: [200, 220] }));
 blocks.push(new Block({ pos: [180, 200] }));
-blocks.push(new Block({ pos: [220, 200] }));*/
+blocks.push(new Block({ pos: [220, 200] }));
 
 var update = function() {
+  ctx.beginPath();
+  ctx.fillStyle = "#FFF";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fill();
   for(var i = 0; i < blocks.length; i++) {
     player.collideBlock(blocks[i]);
     blocks[i].draw();
