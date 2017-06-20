0e51198102c137e47abba6c2ef9e3d3e9688f1d2
TemporalFuzz <vanxmax@gmail.com>
2017-06-19 17:17:09 -0700

    Update setup.js
    Removed a useless variable

--- setup.js
+++ setup.js
@@ -1,6 +1,5 @@
 function setup() {
   var canvas = document.getElementById("canvas");
-  var ctx = canvas.getContext("2d");
   if(window.innerWidth/window.innerHeight < 1.2) {
     canvas.width  = window.innerWidth;
     canvas.height = 5/6 * window.innerWidth;
