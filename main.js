70e0efe6fa19a17ceaa9d4fbfa35afc5e993c1ad
TemporalFuzz <vanxmax@gmail.com>
2017-06-19 21:57:56 -0700

    Update main.js
    Basic functionality test

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
+var player = new Player([200, 200]);
+var blocks = [];
+blocks.push(new Block({ pos: [200, 220] }));
+blocks.push(new Block({ pos: [180, 200] }));
+blocks.push(new Block({ pos: [220, 200] }));
 
+var update = function() {
+  for(var i = 0; i < blocks.length; i++) {
+    player.collideBlock(blocks[i]);
+    blocks[i].draw();
+  }
+  player.display();
+};
+var tick = function() {
+
+};
+setInterval(update, 17);
+//setInterval(tick, 100);
