0e91b360e8e84dcaa8b00ca923462366cbb9f209
TemporalFuzz <vanxmax@gmail.com>
2017-06-21 18:32:57 -0700

    Update buildlevel.js

--- scripts/buildlevel.js
+++ scripts/buildlevel.js
@@ -1,13 +1,20 @@
 var blocks = [];
 
-var buildLevel = function(levelid) {
+var buildLevel = function(levelnum) {
+  var levels;
+  if(!levels) {
+    return;
+  }
   blocks = [];
   
-  var fr = new FileReader();
-  fr.onload = function(event) {
-    var str = event.target.result;
-    console.log(str);
-  };
-  var fn = "levels\\" + levelid + ".txt";
-  fr.readAsText(new File([""], fn));
+  var lvl = levels[levelnum];
+  
+  for(var y = 0; y < lvl.length; y++) {
+    var ln = lvl[y].split(" ");
+    for(var x = 0; x < ln.length; x++) {
+      switch(ln[x]) {
+        case "_bb": blocks.push(new Block({ pos: [x * canvas.width/600 * 20 + 10, y * canvas.height/500 * 20 + 10] })); break;
+      }
+    }
+  }
 };
