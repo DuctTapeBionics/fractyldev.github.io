67bd93bd2a73fa8c8af4557c58b8d71d65b99280
TemporalFuzz <vanxmax@gmail.com>
2017-06-21 18:42:24 -0700

    Update buildlevel.js

--- scripts/buildlevel.js
+++ scripts/buildlevel.js
@@ -7,7 +7,6 @@ var buildLevel = function(levelnum) {
   
   for(var y = 0; y < lvl.length; y++) {
     var ln = lvl[y].split(" ");
-    console.log(ln);
     for(var x = 0; x < ln.length; x++) {
       switch(ln[x]) {
         case "_bb": blocks.push(new Block({ pos: [x * canvas.width/600 * 20 + 10, y * canvas.height/500 * 20 + 10], width: canvas.width/600 * 20, height: canvas.height/500 * 20, })); break;
